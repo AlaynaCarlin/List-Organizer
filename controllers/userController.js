36d54cb8c425ea42760078ce402e91f4fa1c57a4
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-04-29 14:26:23 -0400

    register endpoint complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 // const Express = require('express');
 const router = require('express').Router();
 const { models } = require("../models");
+const { UniqueConstraintError } = require("sequelize/lib/errors");
 
 router.get('/different', (req, res) => {
     res.send("Hey!! Wassup!")
@@ -8,15 +9,32 @@ router.get('/different', (req, res) => {
 
 router.post("/register", async (req, res) => {
 
-   let {username, admin, password} = req.body.user;
+    let { username, admin, password } = req.body.user;
 
-   await models.UsersModel.create({
-       username,
-       admin,
-       password
-   });
+    try {
+        const User = await models.UsersModel.create({
+            username,
+            admin,
+            password
+        });
 
-   res.send("this is our register endpoint");
+        res.status(201).json({
+            message: "user successfully registered",
+            user: User
+        });
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: 'username already in use',
+            });
+        } else {
+            res.status(500).json({
+                message: 'Failed to register user'
+            });
+        }
+    }
 });
+
+// LOGIN ****** 4/30
 
 module.exports = router;
