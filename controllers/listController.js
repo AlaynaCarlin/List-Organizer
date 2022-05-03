b1a33fed1a255e0af848af51859104eccc19ec5d
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-05-03 14:15:10 -0400

    started listcontroller endpoints. owner id null on lists. Drop tables?

--- controllers/listController.js
+++ controllers/listController.js
@@ -7,6 +7,7 @@ router.get('/pretend', validateJWT, (req, res) => {
     res.send("Hey!! Wassup!")
 });
 
+// * Create a List
 router.post('/create', validateJWT, async (req, res) => {
     const {title, date, complete} = req.body.list;
     const {id} = req.user;
@@ -25,8 +26,40 @@ router.post('/create', validateJWT, async (req, res) => {
     models.ListModel.create(listEntry)
 });
 
-// 12.3.4
-// look into the endpoints I need
+// * Update a List
+
+// * Delete a List
+
+// * Get all Lists
+router.get('/', async (req, res) => {
+    try {
+    const entries = await models.ListModel.findAll();
+    res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+// * Get a list by id
+
+// * Get a list by title
+
+// * Get list by user
+router.get("/mine", validateJWT, async (req, res) => {
+    const { id } = req.user;
+
+    try {
+        const userLists = await models.ListModel.findAll({
+            where: {
+                owner: id
+            }
+        });
+        res.status(200).json(userLists);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
 
 
 module.exports = router;
