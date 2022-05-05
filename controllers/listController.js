26857d19f1246e23a1f98b55e480b19f8b94a36e
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-05-05 17:15:01 -0400

    worked on list controller, update not working

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,7 +1,7 @@
 const Express = require('express');
 const router = Express.Router();
 let validateJWT = require("../middleware/validate-jwt");
-const {models} = require('../models');
+const { models } = require('../models');
 
 router.get('/pretend', validateJWT, (req, res) => {
     res.send("Hey!! Wassup!")
@@ -9,40 +9,100 @@ router.get('/pretend', validateJWT, (req, res) => {
 
 // * Create a List
 router.post('/create', validateJWT, async (req, res) => {
-    const {title, date, complete} = req.body.list;
-    const {id} = req.user.id;
-    const listEntry = {
-        title,
-        date,
-        complete,
-        userId: id
-    }
+    const { title, date, complete } = req.body.list;
+   
     try {
-        const newList = await models.ListModel.create(listEntry);
-        res.status(200).json({newList});
+        await models.ListModel.create({
+            title: title,
+            date:date,
+            complete: complete,
+            userId: req.user.id
+        })
+            .then(
+                list => {
+                    res.status(201).json({
+                        list: list,
+                        message: 'successfully posted'
+                    });
+                }
+            )
+        
     } catch (err) {
-        res.status(500).json({error: err});
+        res.status(500).json({ error: err });
     }
-    models.ListModel.create(listEntry)
 });
 
 // * Update a List
+router.put('/update/:entryId', validateJWT, async (req, res) => {
+    const { title, date, complete } = req.body.list;
+    const listId = req.params.entryId;
+    const {id} = req.user;
+
+    const query = {
+        where: {
+            id: listId,
+            userId: id
+        }
+    };
+
+    const updateList = {
+        title: title,
+        date: date,
+        complete: complete
+    };
+
+    try {
+        const update = await models.ListModel.update(updateList, query);
+        res.status(200).json({
+            message: `${update} List successfully updated`,
+            update: updateList,
+            query: query
+        });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
 // * Delete a List
 
 // * Get all Lists
 router.get('/', async (req, res) => {
     try {
-    const entries = await models.ListModel.findAll();
-    res.status(200).json(entries);
+        const entries = await models.ListModel.findAll();
+        res.status(200).json(entries);
     } catch (err) {
         res.status(500).json({ error: err });
     }
 });
 
+// 12.3.4 blue
+
 // * Get a list by id
+router.get('/:id', validateJWT, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const results = await models.ListModel.findAll({
+            where: { id: id }
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
 
 // * Get a list by title
+router.get('/:title', validateJWT, async (req, res) => {
+    const { title } = req.params;
+    try {
+        const results = await models.ListModel.findAll({
+            where: { title: title }
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 
 // * Get list by user
 router.get("/mine", validateJWT, async (req, res) => {
@@ -51,7 +111,7 @@ router.get("/mine", validateJWT, async (req, res) => {
     try {
         const userLists = await models.ListModel.findAll({
             where: {
-                owner: id
+                userId: id
             }
         });
         res.status(200).json(userLists);
